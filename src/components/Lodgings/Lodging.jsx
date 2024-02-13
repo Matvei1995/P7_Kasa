5e72f8ce0821b2f172fc14505b934324102fbd52
Matvei <matvei.tabelev@gmail.com>
2024-02-13 14:03:58 +0100

    deploy

--- src/components/Lodgings/Lodging.jsx
+++ src/components/Lodgings/Lodging.jsx
@@ -2,38 +2,43 @@ import React, { useEffect, useState } from 'react';
 
 import LodgingCards from '../LodgingCards/LodgingCards';
 import './Lodging.scss';
-import logements from "../listLodgings.json";
-function Lodging() {
-  const [lodgings, setLodgings] = useState([]);
 
-  useEffect(fetchLodgings, []);
+function Lodging() {
+  const [homes, setLodgings] = useState([]);
+  console.log(homes);
+   useEffect(fetchLodgings, []);
+
  function fetchLodgings() {
-    fetch('/listLodgings.json')
+    fetch("/listLodgings.json")
       .then((response) =>{ 
-          console.log(response);
-        response.json();
+        return response.json();
       })
-      .then((response) => setLodgings(response))
+      .then((response) => {
+        console.log(response);
+        setLodgings(response)
+      
+      })
       .catch((error)=>{
         console.log(error);
       });
     
   }
   
- 
-  return (
-    
-    <div className="lodging__grid">
-    
-        {lodgings.map((lodging) => (
-          <LodgingCards
-            key={lodging.id}
-            id={lodging.id}
-            title={lodging.title}
-            imageUrl={lodging.cover}
-          />
-        ))}
+  const maisons = homes.map((lodging) => {
+    return (
+      <LodgingCards
+      key={lodging.id}
+      id={lodging.id}
+      title={lodging.title}
+      imageUrl={lodging.cover}
+      />
       
+          )
+        }
+  );
+  return (
+    <div className="lodging__grid">
+      {maisons}
     </div>
   );
 }
